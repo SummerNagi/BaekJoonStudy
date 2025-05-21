bc24e2f34ce300adac428bd6530d339b995800eb
shinhong-gyu <shin02198@gmail.com>
2025-05-22 00:34:30 +0900

    Init

---
+++ SummerNagi/SHK/11659.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// 구간 합 배열
+int S[100001] = {};
+
+int main()
+{
+	int N, M, num, i, j, idx;
+	scanf("%d%d", &N, &M);
+
+	for (idx = 1; idx <= N; idx++)
+	{
+		scanf("%d", &num);
+		S[idx] = S[idx - 1] + num;
+	}
+	for (idx = 0; idx < M; idx++)
+	{
+		scanf("%d%d", &i, &j);
+		printf("%d\n", S[j] - S[i - 1]);
+	}
+}
