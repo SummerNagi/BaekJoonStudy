01fdff58268f6c04f2ba34cf482e1a75498bb120
nimnoeyheel <hyeonmin1012@gmail.com>
2025-05-22 15:59:20 +0900

    LHM - 1546
    평균

--- SummerNagi/LHM/11720.cpp
+++ SummerNagi/LHM/11720.cpp
@@ -1,20 +1,20 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-	int n;
-	string numbers;
-	cin >> n;
-	cin >> numbers;
-
-	int result = 0;
-
-	for (int i = 0; i < numbers.length(); i++)
-	{
-		result += numbers[i] - '0';
-	}
-
-	cout << result;
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	string numbers;
+//	cin >> n;
+//	cin >> numbers;
+//
+//	int result = 0;
+//
+//	for (int i = 0; i < numbers.length(); i++)
+//	{
+//		result += numbers[i] - '0';
+//	}
+//
+//	cout << result;
+//}
---
+++ SummerNagi/LHM/1546.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	vector<double> scores(n);
+	double max = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> scores[i];
+		if (max < scores[i])
+		{
+			max = scores[i];
+		}
+	}
+
+	double sum = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		scores[i] = (scores[i] / max) * 100;
+		sum += scores[i];
+	}
+
+	cout << sum / n;
+}
