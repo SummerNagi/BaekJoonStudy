ae867b10342c2394dbb20ee7b18955892e5e3402
shinhong-gyu <shin02198@gmail.com>
2025-05-22 09:28:46 +0900

    신홍규 - 11722

--- SummerNagi/SHK/11659.cpp
+++ SummerNagi/SHK/11659.cpp
@@ -1,21 +1,21 @@
-#include <stdio.h>
-
-// ±¸°£ ÇÕ ¹è¿­
-int S[100001] = {};
-
-int main()
-{
-	int N, M, num, i, j, idx;
-	scanf("%d%d", &N, &M);
-
-	for (idx = 1; idx <= N; idx++)
-	{
-		scanf("%d", &num);
-		S[idx] = S[idx - 1] + num;
-	}
-	for (idx = 0; idx < M; idx++)
-	{
-		scanf("%d%d", &i, &j);
-		printf("%d\n", S[j] - S[i - 1]);
-	}
-}
+ï»¿// #include <stdio.h>
+// 
+// // êµ¬ê°„ í•© ë°°ì—´
+// int S[100001] = {};
+// 
+// int main()
+// {
+// 	int N, M, num, i, j, idx;
+// 	scanf("%d%d", &N, &M);
+// 
+// 	for (idx = 1; idx <= N; idx++)
+// 	{
+// 		scanf("%d", &num);
+// 		S[idx] = S[idx - 1] + num;
+// 	}
+// 	for (idx = 0; idx < M; idx++)
+// 	{
+// 		scanf("%d%d", &i, &j);
+// 		printf("%d\n", S[j] - S[i - 1]);
+// 	}
+// }
