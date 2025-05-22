fc562ef3b8cd7ebe874a10a5b462c90d7f3962d3
flatload <jky72ta@gmail.com>
2025-05-22 15:44:36 +0900

    csr-1920

---
+++ SummerNagi/CSL/1920.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    std::cin.tie(0);
+    std::cin.sync_with_stdio(false);
+    std::ios_base::sync_with_stdio(false);
+    int N = 0;
+    cin >> N;
+    vector<int> rng(N, 0);
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> rng[i];
+    }
+    sort(rng.begin(), rng.end());
+
+    int M = 0;
+    cin >> M;
+    for (int i = 0; i < M; ++i)
+    {
+        int num = 0;
+        cin >> num;
+        vector<int>::iterator it = lower_bound(rng.begin(), rng.end(), num);
+        if (it != rng.end() && *it == num)
+            cout << 1 << '\n';
+        else
+            cout << 0 << '\n';
+    }
+    return (0);
+}
