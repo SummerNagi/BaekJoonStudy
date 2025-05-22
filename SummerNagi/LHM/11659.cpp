017394d140fbfbd06317170dfc8201171e7265eb
nimnoeyheel <hyeonmin1012@gmail.com>
2025-05-22 15:06:58 +0900

    LHM - 11720
    숫자의 합

--- SummerNagi/LHM/11659.cpp
+++ SummerNagi/LHM/11659.cpp
@@ -1,29 +1,29 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	int n, m;
-	cin >> n >> m;
-	int S[100000] = {};
-	for (int i = 1; i <= n; i++)
-	{
-		int temp;
-		cin >> temp;
-		// 합 배열
-		S[i] = S[i-1] + temp;
-	}
-
-	for (int i = 0; i < m; i++)
-	{
-		int start, end;
-		cin >> start >> end;
-		cout << S[end] - S[start-1] << "\n";
-	}
-}
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//	cout.tie(NULL);
+//
+//	int n, m;
+//	cin >> n >> m;
+//	int S[100000] = {};
+//	for (int i = 1; i <= n; i++)
+//	{
+//		int temp;
+//		cin >> temp;
+//		// 합 배열
+//		S[i] = S[i-1] + temp;
+//	}
+//
+//	for (int i = 0; i < m; i++)
+//	{
+//		int start, end;
+//		cin >> start >> end;
+//		cout << S[end] - S[start-1] << "\n";
+//	}
+//}
