29a0af50d4577ee8743c0f6dc888197c3e349d2e
shinhong-gyu <shin02198@gmail.com>
2025-05-24 18:41:00 +0900

    SHK - 11003

---
+++ SummerNagi/SHK/11003.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <stdio.h>
+#include <deque>
+
+using namespace std;
+
+int a[5000010];
+deque<pair<int, int>> d;
+
+int main() 
+{
+
+	int N, L;
+	scanf("%d %d", &N, &L);
+
+	for (int i = 0; i < N; i++)
+	{
+		scanf("%d", &a[i]);
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		if (!d.empty() && d.front().second < i - L + 1)
+		{
+			d.pop_front();
+		}
+
+		while (!d.empty() && a[i] < d.back().first)
+		{
+			d.pop_back();
+		}
+
+		d.push_back(make_pair(a[i], i));
+
+		printf("%d ", d.front().first);
+	}
+}
