d085866fec386603635db624073a642bf15581d3
yagae12 <1kant11071@gmail.com>
2025-05-25 20:31:23 +0900

    2750

---
+++ SummerNagi/KGW/11003.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <deque>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio;
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, L;
+	cin >> N >> L;
+	deque<pair<int, int>> dq;
+	vector<int>A(N);
+
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> A[i];
+
+		while (!dq.empty() && dq.back().first > A[i])dq.pop_back();
+
+		dq.emplace_back(A[i], i);
+
+		while (!dq.empty() && dq.front().second < i - L)dq.pop_front();
+
+		cout << dq.front().first << " ";
+
+	}
+
+	return 0;
+}
---
+++ SummerNagi/KGW/2750.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, cnt;
+
+
+int main()
+{
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int a[1001];
+	cin >> N;
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> a[i];
+	}
+
+	sort(a, a + N);
+
+	for (int i = 0; i < N; ++i)
+	{
+		cout << a[i] << " ";
+	}
+
+	return 0;
+}
