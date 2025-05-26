9c1d88d4efb8bc81fb6f4588bf9f77a15e1b541e
nimnoeyheel <hyeonmin1012@gmail.com>
2025-05-26 09:39:31 +0900

    LHM - 12891
    DNA 비밀번호

--- SummerNagi/LHM/12891.cpp
+++ SummerNagi/LHM/12891.cpp
@@ -1,11 +1,97 @@
-﻿//#include <iostream>
-//
-//using namespace std;
-//
-//int main()
-//{
-//	int n, l;
-//	cin >> n >> l;
-//
-//	return;
-//}
+﻿#include <iostream>
+
+using namespace std;
+
+int checkArr[4];		// 비밀번호 체크 배열
+int currentArr[4];		// 현재 상태 배열
+int checkSecret = 0;	// 유효 비밀번호 판단 변수
+
+// 새로 들어온 문자 처리
+void Add(char c)
+{
+	switch (c)
+	{
+		case 'A':
+			currentArr[0]++;
+			if (currentArr[0] == checkArr[0]) checkSecret++;
+			break;
+		case 'C':
+			currentArr[1]++;
+			if (currentArr[1] == checkArr[1]) checkSecret++;
+			break;
+		case 'G':
+			currentArr[2]++;
+			if (currentArr[2] == checkArr[2]) checkSecret++;
+			break;
+		case 'T':
+			currentArr[3]++;
+			if (currentArr[3] == checkArr[3]) checkSecret++;
+			break;
+	}
+}
+
+// 제거할 문자 처리
+void Remove(char c)
+{
+	switch (c)
+	{
+		case 'A':
+			if (currentArr[0] == checkArr[0]) checkSecret--;
+			currentArr[0]--;
+			break;
+		case 'C':
+			if (currentArr[1] == checkArr[1]) checkSecret--;
+			currentArr[1]--;
+			break;
+		case 'G':
+			if (currentArr[2] == checkArr[2]) checkSecret--;
+			currentArr[2]--;
+			break;
+		case 'T':
+			if (currentArr[3] == checkArr[3]) checkSecret--;
+			currentArr[3]--;
+			break;
+	}
+}
+
+// DNA 비밀번호
+int main()
+{
+	int s;			// 문자열 길이
+	int p;			// 부분 문자열 길이
+	string a;		// 문자열
+	int result = 0;
+
+	cin >> s >> p;
+	cin >> a;
+	
+	for (int i = 0; i < 4; ++i)
+	{
+		cin >> checkArr[i];
+		if (checkArr[i] == 0) checkSecret++;
+	} 
+
+	// 초기 P 부분 문자열
+	for (int i = 0; i < p; ++i)
+	{
+		Add(a[i]);
+	}
+	
+	// 4개 유효 갯수 모두 충족했을 때
+	if(checkSecret == 4) result++;
+
+	// 슬라이딩 윈도우 부분 처리
+	for (int i = p; i < s; ++i)
+	{
+		int j = i - p;
+		Add(a[i]);
+		Remove(a[j]);
+
+		// 4개 유효 갯수 모두 충족했을 때
+		if (checkSecret == 4) result++;
+	}
+
+	cout << result;
+
+	return 0;
+}
--- SummerNagi/LHM/2018.cpp
+++ SummerNagi/LHM/2018.cpp
@@ -1,40 +1,40 @@
-﻿#include <iostream>
-
-using namespace std;
-
-int main()
-{
-	int n;
-	cin >> n;
-	int count = 1;
-	int start = 1;
-	int end = 1;
-	int sum = 1;
-
-	while (end != n)
-	{
-		// 연속된 수의 합이 n과 같을 때
-		if (sum == n)
-		{
-			count++;
-			end++;
-			sum = sum + end;
-		}
-		// 연속된 수의 합이 n보다 클 때
-		else if (sum > n)
-		{
-			sum = sum - start;
-			start++;
-		}
-		// 연속된 수의 합이 n보다 작을 때
-		else
-		{
-			end++;
-			sum = sum + end;
-		}
-	}
-
-	cout << count << endl;
-
-	return 0;
-}
+﻿//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	int count = 1;
+//	int start = 1;
+//	int end = 1;
+//	int sum = 1;
+//
+//	while (end != n)
+//	{
+//		// 연속된 수의 합이 n과 같을 때
+//		if (sum == n)
+//		{
+//			count++;
+//			end++;
+//			sum = sum + end;
+//		}
+//		// 연속된 수의 합이 n보다 클 때
+//		else if (sum > n)
+//		{
+//			sum = sum - start;
+//			start++;
+//		}
+//		// 연속된 수의 합이 n보다 작을 때
+//		else
+//		{
+//			end++;
+//			sum = sum + end;
+//		}
+//	}
+//
+//	cout << count << endl;
+//
+//	return 0;
+//}
