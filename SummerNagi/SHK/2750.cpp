caf8f3d830120495f884635f4644c54800fa77c1
shinhong-gyu <shin02198@gmail.com>
2025-05-26 14:10:25 +0900

    SHK-11724

--- SummerNagi/SHK/2750.cpp
+++ SummerNagi/SHK/2750.cpp
@@ -1,40 +1,40 @@
-#include <iostream>
-#include <map>
-
-using namespace std;
-
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	int T;
-	cin >> T;
-
-	map<int, int> m;
-
-	for (int i = 0; i < T; i++)
-	{
-		int temp;
-		cin >> temp;
-		m[temp] = 1;
-	}
-
-	for (map<int, int>::iterator it = m.begin(); it != m.end(); it++)
-	{
-		if (it->second > 1)
-		{
-			for (int i = 0; i < it->second; i++)
-			{
-				cout << it->first << "\n";
-			}
-		}
-		else
-		{
-			cout << it->first << "\n";
-		}
-	}
-
-
-}
+// #include <iostream>
+// #include <map>
+// 
+// using namespace std;
+// 
+// int main()
+// {
+// 	ios::sync_with_stdio(false);
+// 	cin.tie(NULL);
+// 	cout.tie(NULL);
+// 
+// 	int T;
+// 	cin >> T;
+// 
+// 	map<int, int> m;
+// 
+// 	for (int i = 0; i < T; i++)
+// 	{
+// 		int temp;
+// 		cin >> temp;
+// 		m[temp] = 1;
+// 	}
+// 
+// 	for (map<int, int>::iterator it = m.begin(); it != m.end(); it++)
+// 	{
+// 		if (it->second > 1)
+// 		{
+// 			for (int i = 0; i < it->second; i++)
+// 			{
+// 				cout << it->first << "\n";
+// 			}
+// 		}
+// 		else
+// 		{
+// 			cout << it->first << "\n";
+// 		}
+// 	}
+// 
+// 
+// }
