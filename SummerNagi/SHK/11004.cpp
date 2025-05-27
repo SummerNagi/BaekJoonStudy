8dc1c957f5d876b33d4f6877b37bf3465b96f2de
shinhong-gyu <shin02198@gmail.com>
2025-05-27 11:55:58 +0900

    SHK - 2178, 18111

--- SummerNagi/SHK/11004.cpp
+++ SummerNagi/SHK/11004.cpp
@@ -1,95 +1,95 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-int Partition(vector<int>& arr, int s, int e)
-{
-	if (arr.size() == 2)
-	{
-		if (arr[0] > arr[1])
-		{
-			swap(arr[0], arr[1]);
-		}
-		return 1;
-	}
-
-	int low = s + 1;
-	int high = e;
-	int p = (s + e) / 2;
-
-	swap(arr[s], arr[p]);
-
-	int pivot = arr[s];
-
-	while (low <= high)
-	{
-		while (arr[high] > pivot && low <= high)
-		{
-			high--;
-		}
-
-		while (arr[low] < pivot && low <= high)
-		{
-			low++;
-		}
-
-		if (low < high)
-		{
-			swap(arr[low], arr[high]);
-			low++;
-			high--;
-		}
-		else
-		{
-			break;
-		}
-	}
-
-	arr[s] = arr[high];
-	arr[high] = pivot;
-
-	return high;
-}
-
-void QuickSort(vector<int>& arr, int start, int end, int K)
-{
-	int pivot = Partition(arr, start, end);
-
-	if (pivot == K)
-	{
-		return;
-	}
-
-	else if (pivot < K)
-	{
-		QuickSort(arr, start, pivot - 1, K);
-	}
-
-	else
-	{
-		QuickSort(arr, pivot + 1, end, K);
-	}
-}
-
-
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	int N, K;
-	cin >> N >> K;
-
-	vector<int> arr(N, 0);
-
-	for (int i = 0; i < N; i++)
-	{
-		cin >> arr[i];
-	}
-
-	QuickSort(arr, 0, N - 1, K - 1);
-
-	cout << arr[K - 1];
-}
+// #include <iostream>
+// #include <vector>
+// 
+// using namespace std;
+// 
+// int Partition(vector<int>& arr, int s, int e)
+// {
+// 	if (arr.size() == 2)
+// 	{
+// 		if (arr[0] > arr[1])
+// 		{
+// 			swap(arr[0], arr[1]);
+// 		}
+// 		return 1;
+// 	}
+// 
+// 	int low = s + 1;
+// 	int high = e;
+// 	int p = (s + e) / 2;
+// 
+// 	swap(arr[s], arr[p]);
+// 
+// 	int pivot = arr[s];
+// 
+// 	while (low <= high)
+// 	{
+// 		while (arr[high] > pivot && low <= high)
+// 		{
+// 			high--;
+// 		}
+// 
+// 		while (arr[low] < pivot && low <= high)
+// 		{
+// 			low++;
+// 		}
+// 
+// 		if (low < high)
+// 		{
+// 			swap(arr[low], arr[high]);
+// 			low++;
+// 			high--;
+// 		}
+// 		else
+// 		{
+// 			break;
+// 		}
+// 	}
+// 
+// 	arr[s] = arr[high];
+// 	arr[high] = pivot;
+// 
+// 	return high;
+// }
+// 
+// void QuickSort(vector<int>& arr, int start, int end, int K)
+// {
+// 	int pivot = Partition(arr, start, end);
+// 
+// 	if (pivot == K)
+// 	{
+// 		return;
+// 	}
+// 
+// 	else if (pivot < K)
+// 	{
+// 		QuickSort(arr, start, pivot - 1, K);
+// 	}
+// 
+// 	else
+// 	{
+// 		QuickSort(arr, pivot + 1, end, K);
+// 	}
+// }
+// 
+// 
+// int main()
+// {
+// 	ios::sync_with_stdio(false);
+// 	cin.tie(NULL);
+// 	cout.tie(NULL);
+// 
+// 	int N, K;
+// 	cin >> N >> K;
+// 
+// 	vector<int> arr(N, 0);
+// 
+// 	for (int i = 0; i < N; i++)
+// 	{
+// 		cin >> arr[i];
+// 	}
+// 
+// 	QuickSort(arr, 0, N - 1, K - 1);
+// 
+// 	cout << arr[K - 1];
+// }
---
+++ SummerNagi/SHK/18111.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, M, B;
+	cin >> N >> M >> B;
+
+	vector<vector<int>> ground(N + 1, vector<int>(M + 1));
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			cin >> ground[i][j];
+		}
+	}
+
+	int minTime = 2147483647;
+	int minHeight = 0;
+	for (int targetHeight = 0; targetHeight <= 256; targetHeight++)
+	{
+		int time = 0;
+		int block = B;
+
+		for (int i = 0; i < N; i++)
+		{
+			for (int j = 0; j < M; j++)
+			{
+				int curHeight = ground[i][j];
+				if (curHeight < targetHeight)
+				{
+					int gap = targetHeight - curHeight;
+					block -= gap;
+					time += gap;
+				}
+				else if (curHeight > targetHeight)
+				{
+					int gap = curHeight - targetHeight;
+
+					time += gap * 2;
+					block += gap;
+				}
+			}
+		}
+
+		if (block < 0)
+		{
+			continue;
+		}
+
+		if (time < minTime)
+		{
+			minTime = time;
+			minHeight = targetHeight;
+		}
+	}
+
+	cout << minTime << " " << minHeight;
+}
