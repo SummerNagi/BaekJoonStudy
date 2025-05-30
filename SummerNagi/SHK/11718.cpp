95144b91a83e483e1d378b8a691a9b7e8001659b
shinhong-gyu <shin02198@gmail.com>
2025-05-30 11:17:41 +0900

    SHK - 11403

--- SummerNagi/SHK/11718.cpp
+++ SummerNagi/SHK/11718.cpp
@@ -1,27 +1,27 @@
-#include <vector>
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	while (1)
-	{
-		string temp;
-
-		getline(cin,temp);
-		
-		if (temp.size() == 0)
-		{
-			break;
-		}
-
-		cout << temp << "\n";
-	}
-
-}
+// #include <vector>
+// #include <iostream>
+// #include <string>
+// 
+// using namespace std;
+// 
+// int main()
+// {
+// 	ios::sync_with_stdio(false);
+// 	cin.tie(NULL);
+// 	cout.tie(NULL);
+// 
+// 	while (1)
+// 	{
+// 		string temp;
+// 
+// 		getline(cin,temp);
+// 		
+// 		if (temp.size() == 0)
+// 		{
+// 			break;
+// 		}
+// 
+// 		cout << temp << "\n";
+// 	}
+// 
+// }
