d854eca430463c3dd87aa184afa2d157d01fc7a7
shinhong-gyu <shin02198@gmail.com>
2025-06-19 10:55:50 +0900

    SHK - 11725

--- SummerNagi/SHK/7662.cpp
+++ SummerNagi/SHK/7662.cpp
@@ -1,98 +1,98 @@
-#include <iostream>
-#include <vector>
-#include <cmath>
-#include <queue>
-#include <map>
-
-
-using namespace std;
-
-
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	int T;
-	cin >> T;
-
-	while (T--)
-	{
-		int K;
-
-		cin >> K;
-
-		priority_queue<int> maxpq;
-		priority_queue<int, vector<int>, greater<int>> minpq;
-		map<int, int> m;
-
-		for (int i = 0; i < K; i++)
-		{
-			char oper;
-			int n;
-
-			cin >> oper >> n;
-
-			if (oper == 'I')
-			{
-				minpq.push(n);
-				maxpq.push(n);
-				m[n]++;
-			}
-			else
-			{
-				if (m.empty())
-				{
-					continue;
-				}
-
-				if (n == -1)
-				{
-					while (!minpq.empty() && m[minpq.top()] == 0)
-					{
-						minpq.pop();
-					}
-					if (!minpq.empty())
-					{
-						m[minpq.top()]--;
-						minpq.pop();
-					}
-				}
-				else
-				{
-					while (!maxpq.empty() && m[maxpq.top()] == 0)
-					{
-						maxpq.pop();
-					}
-					if (!maxpq.empty())
-					{
-						m[maxpq.top()]--;
-						maxpq.pop();
-					}
-				}
-			}
-		}
-
-		while (!minpq.empty() && m[minpq.top()] == 0)
-		{
-			minpq.pop();
-		}
-
-		while (!maxpq.empty() && m[maxpq.top()] == 0)
-		{
-			maxpq.pop();
-		}
-
-		if (minpq.empty() || maxpq.empty())
-		{
-			cout << "EMPTY" << '\n';
-			continue;
-		}
-
-		cout << maxpq.top() << ' ' << minpq.top() << '\n';
-	}
-
-}
-
-
+// #include <iostream>
+// #include <vector>
+// #include <cmath>
+// #include <queue>
+// #include <map>
+// 
+// 
+// using namespace std;
+// 
+// 
+// int main()
+// {
+// 	ios::sync_with_stdio(false);
+// 	cin.tie(NULL);
+// 	cout.tie(NULL);
+// 
+// 	int T;
+// 	cin >> T;
+// 
+// 	while (T--)
+// 	{
+// 		int K;
+// 
+// 		cin >> K;
+// 
+// 		priority_queue<int> maxpq;
+// 		priority_queue<int, vector<int>, greater<int>> minpq;
+// 		map<int, int> m;
+// 
+// 		for (int i = 0; i < K; i++)
+// 		{
+// 			char oper;
+// 			int n;
+// 
+// 			cin >> oper >> n;
+// 
+// 			if (oper == 'I')
+// 			{
+// 				minpq.push(n);
+// 				maxpq.push(n);
+// 				m[n]++;
+// 			}
+// 			else
+// 			{
+// 				if (m.empty())
+// 				{
+// 					continue;
+// 				}
+// 
+// 				if (n == -1)
+// 				{
+// 					while (!minpq.empty() && m[minpq.top()] == 0)
+// 					{
+// 						minpq.pop();
+// 					}
+// 					if (!minpq.empty())
+// 					{
+// 						m[minpq.top()]--;
+// 						minpq.pop();
+// 					}
+// 				}
+// 				else
+// 				{
+// 					while (!maxpq.empty() && m[maxpq.top()] == 0)
+// 					{
+// 						maxpq.pop();
+// 					}
+// 					if (!maxpq.empty())
+// 					{
+// 						m[maxpq.top()]--;
+// 						maxpq.pop();
+// 					}
+// 				}
+// 			}
+// 		}
+// 
+// 		while (!minpq.empty() && m[minpq.top()] == 0)
+// 		{
+// 			minpq.pop();
+// 		}
+// 
+// 		while (!maxpq.empty() && m[maxpq.top()] == 0)
+// 		{
+// 			maxpq.pop();
+// 		}
+// 
+// 		if (minpq.empty() || maxpq.empty())
+// 		{
+// 			cout << "EMPTY" << '\n';
+// 			continue;
+// 		}
+// 
+// 		cout << maxpq.top() << ' ' << minpq.top() << '\n';
+// 	}
+// 
+// }
+// 
+// 
