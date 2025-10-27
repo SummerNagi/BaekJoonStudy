71513e34563c62cd7d340611f4814f183f1ea3ff
shinhong-gyu <113535319+shinhong-gyu@users.noreply.github.com>
2025-10-27 18:33:56 +0900

    SHK - 10830

--- SummerNagi/SHK/9372.cpp
+++ SummerNagi/SHK/9372.cpp
@@ -1,33 +1,33 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <numeric>
-
-using namespace std;
-
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	int T;
-	cin >> T;
-
-	while (T--)
-	{
-		int N, M;
-		cin >> N >> M;
-
-		for (int i = 0; i < M; i++)
-		{
-			int a, b;
-			cin >> a >> b;
-		}
-
-
-		cout << N - 1 << "\n";
-	}
-
-
-
-}
+//#include <iostream>
+//#include <vector>
+//#include <queue>
+//#include <numeric>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//
+//	int T;
+//	cin >> T;
+//
+//	while (T--)
+//	{
+//		int N, M;
+//		cin >> N >> M;
+//
+//		for (int i = 0; i < M; i++)
+//		{
+//			int a, b;
+//			cin >> a >> b;
+//		}
+//
+//
+//		cout << N - 1 << "\n";
+//	}
+//
+//
+//
+//}
