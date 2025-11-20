f5e24ae6d87496405d04b9f9d5b61446a503a8c6
shinhong-gyu <shin02198@gmail.com>
2025-11-20 14:36:12 +0900

    SHK - 2720

--- SummerNagi/SHK/2720.cpp
+++ SummerNagi/SHK/2720.cpp
@@ -1,31 +1,31 @@
-#include < iostream>
-
-using namespace std;
-
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	int T;
-	cin >> T;
-
-	while (T--)
-	{
-		int N;
-		cin >> N;
-
-		int q = 0;
-		int d = 0;
-		int n = 0;
-		int p = 0;
-
-		q = N / 25;
-		N %= 25;
-		d = N / 10;
-		N %= 10;
-		n = N / 5;
-		N %= 5;
-		p = N;
-	}
-}
+//#include < iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL);
+//
+//	int T;
+//	cin >> T;
+//
+//	while (T--)
+//	{
+//		int N;
+//		cin >> N;
+//
+//		int q = 0;
+//		int d = 0;
+//		int n = 0;
+//		int p = 0;
+//
+//		q = N / 25;
+//		N %= 25;
+//		d = N / 10;
+//		N %= 10;
+//		n = N / 5;
+//		N %= 5;
+//		p = N;
+//	}
+//}
