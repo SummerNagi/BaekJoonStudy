fde103631f3bd6657f7560ed01dfb96357b5f348
carttman <chj990920@gmail.com>
2025-12-26 20:29:07 +0900

    CHJ - 1717

---
+++ SummerNagi/CHJ/Gold/1717_집합의 표현.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+static vector<int> parent;
+int find(int a)
+{
+	if (a == parent[a])
+		return a;
+
+	return parent[a] = find(parent[a]);
+}
+
+void unionfunc(int a, int b)
+{
+	a = find(a);
+	b = find(b);
+
+	if (a != b)
+		parent[b] = a;
+}
+
+bool checkSame(int a, int b)
+{
+	a = find(a);
+	b = find(b);
+
+	if (a == b)
+		return true;
+
+	return false;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n, m;
+	cin >> n >> m;
+
+	parent.resize(n + 1);
+
+	for (int i = 0; i <= n; i++)
+		parent[i] = i;
+
+	for (int i = 0; i < m; i++)
+	{
+		int question, a, b;
+		cin >> question >> a >> b;
+
+		if (!question)
+			unionfunc(a, b);
+		else
+			if (checkSame(a, b))
+				cout << "YES" << '\n';
+			else
+				cout << "NO" << '\n';
+	}
+
+	return 0;
+}
